fede9c824f2e1bb992ed0c6dac9feba6f046c385
Spheali <cowan.gavin04@gmail.com>
2024-12-06 09:53:49 -0500

    Added options and prints
    Implemented Print item, print poke, and menu

--- pokedex.js
+++ pokedex.js
@@ -8,7 +8,10 @@ const pokedex = new Pokedex();
 function showMenu() {
     //display all menu options
     console.log("Here are your options: ");
-    console.log("Option1\nOption2");
+    console.log("1: Search for a Pokemon.");
+    console.log("2: Search for an Item.");
+    console.log("3: Search for a Pokemon Move.");
+    console.log("4: End search.")
 }
 
 function prompt(cb) {
@@ -23,10 +26,23 @@ function searchPoke(term) {
     //if valid response is received it will call printPoke(json) with the
     //json to print out the name, weight, height, base experience, and all the moves for that
     //Pokemon. It will then call run() again to reprompt.
+
+    run();
 }
 
 function printPoke(json) {
     //print data from pokemon in neat and clean way
+    const temp = JSON.parse(json);
+    console.log("Name: " + temp.name);
+    console.log("Pokedex Number: " + temp.id);
+    console.log("Height: " + temp.height + " ");
+    console.log("Weight: " + (temp.weight/10) * 2.205 + " lbs");
+    console.log("Species: " + temp.species.name);
+    console.log("Abilities:");
+    for(const key of temp[abilities]) {
+        const name = temp[abilities][key][ability][name];
+        console.log("\t" + name);
+    }
 }
 
 function searchItem(term) {
@@ -34,15 +50,24 @@ function searchItem(term) {
     //searches the item endpoint for an item.
     //calls corresponding printItem(json) method
     //calls run() to reprompt
+
+    run();
 }
 
 function printItem(json) {
     //prints item data neatly pick 4 fields to display from endpoint's data
+    const temp = JSON.parse(json);
+    console.log("Item name: " + temp.name);
+    console.log("Item type: " + temp.category.name);
+    console.log("Item cost: " + temp.cost);
+    console.log("Item effect: " + temp[effect_entries][0][effect]);
 }
 
 function searchMove(term) {
     //works exactly like searchPoke() except searches move endpoint for a move
     //calls corresponding printMove(json) method
+
+    run();
 }
 
 function printMove(json) {
@@ -63,6 +88,9 @@ function run() {
                 break;
             case "3" :
                 prompt(searchMove);
+                break;
+            case "4" :
+                exit("Quit out");
                 break;
             default:
                 console.log("Please choose from the list.");

41442c58f6a92c0d0c03cc07c5b02eae6d1cd0fa
Gavin Morrow <gavin.morrow@spartannash.com>
2024-12-06 17:04:44 -0500

    searchPoke, searchItem (wip)

--- pokedex.js
+++ pokedex.js
@@ -4,6 +4,7 @@ import readline from "readline";
 const rl = readline.createInterface(process.stdin, process.stdout);
 
 const pokedex = new Pokedex();
+run(); //run our program
 
 function showMenu() {
     //display all menu options
@@ -11,7 +12,7 @@ function showMenu() {
     console.log("1: Search for a Pokemon.");
     console.log("2: Search for an Item.");
     console.log("3: Search for a Pokemon Move.");
-    console.log("4: End search.")
+    console.log("4: End search.");
 }
 
 function prompt(cb) {
@@ -21,51 +22,57 @@ function prompt(cb) {
     });
 }
 
-function searchPoke(term) {
-    //query API for particular pokemon (passed as term)
-    //if valid response is received it will call printPoke(json) with the
-    //json to print out the name, weight, height, base experience, and all the moves for that
-    //Pokemon. It will then call run() again to reprompt.
-
-    run();
+async function searchPoke(term) {
+    //queries API for what poke user decides to enter
+    try {
+        const response = await pokedex.getPokemonByName(term); //query API for a response
+        printPoke(response); //print response in a neat way
+    } catch (error) {
+        console.error("Error:", error);
+    } finally {
+        run();
+    }
 }
 
 function printPoke(json) {
     //print data from pokemon in neat and clean way
-    const temp = JSON.parse(json);
-    console.log("Name: " + temp.name);
-    console.log("Pokedex Number: " + temp.id);
-    console.log("Height: " + temp.height/10 + " m");
-    console.log("Weight: " + temp.weight/10 + " kg");
-    console.log("Type: ");
-    for(const key of temp.types) {
+    console.log("Name: " + json.name);
+    console.log("Pokedex Number: " + json.id);
+    console.log("Height: " + json.height / 10 + " m");
+    console.log("Weight: " + json.weight / 10 + " kg");
+
+    console.log("Type/s:");
+    for (const key of json.types) {
         const type = key.type.name;
         console.log("\t" + type);
     }
+
     console.log("Abilities:");
-    for(const key of temp.abilities) {
+    for (const key of json.abilities) {
         const abil_name = key.ability.name;
         console.log("\t" + abil_name);
     }
 }
 
-function searchItem(term) {
-    //works exactly like searchPoke() function, except
-    //searches the item endpoint for an item.
-    //calls corresponding printItem(json) method
-    //calls run() to reprompt
-
-    run();
+async function searchItem(term) {
+    //queries API for what poke user decides to enter
+    try {
+        const response = await pokedex.getItemByName(term); //query API for a response
+        printPoke(response); //print response in a neat way
+    } catch (error) {
+        console.error("Error:", error);
+    } finally {
+        run();
+    }
 }
 
 function printItem(json) {
     //prints item data neatly pick 4 fields to display from endpoint's data
-    const temp = JSON.parse(json);
-    console.log("Item name: " + temp.name);
-    console.log("Item type: " + temp.category.name);
-    console.log("Item cost: " + temp.cost);
+    console.log("Item name: " + json.name);
+    console.log("Item type: " + json.category.name);
+    console.log("Item cost: " + json.cost);
     console.log("Effect:");
-    for(const key of temp.effect_entries) {
+    for (const key of json.effect_entries) {
         const effect = key.effect.name;
         console.log("\t" + effect);
     }
@@ -81,15 +88,15 @@ function searchMove(term) {
 
 function printMove(json) {
     //prints move data in neat way
-    const temp = JSON.parse(json);
-    console.log("Name: " + temp.name);
-    console.log("Power: " + temp.power);
-    console.log("Accuracy: " + temp.accuracy);
-    console.log("PP: " + temp.pp);
-    console.log("Type: " + temp.type.name);
-    console.log("Damage type: " + temp.damage_class.name);
+
+    console.log("Name: " + json.name);
+    console.log("Power: " + json.power);
+    console.log("Accuracy: " + json.accuracy);
+    console.log("PP: " + json.pp);
+    console.log("Type: " + json.type.name);
+    console.log("Damage type: " + json.damage_class.name);
     console.log("Description:");
-    for(const key of temp.effect_entries) {
+    for (const key of json.effect_entries) {
         const effect = key.effect.name;
         console.log("\t" + effect);
     }
@@ -100,17 +107,17 @@ function run() {
     //we will call prompt function and pass it to the name of the function we want to use
     showMenu();
     rl.question("What would you like to do? ", (response) => {
-        switch(response) {
-            case "1" : // Searches for Pokemon on 1
+        switch (response) {
+            case "1": // Searches for Pokemon on 1
                 prompt(searchPoke);
                 break;
-            case "2" : // Searches for Items on 2
+            case "2": // Searches for Items on 2
                 prompt(searchItem);
                 break;
-            case "3" : // Searches for Moves on 3
+            case "3": // Searches for Moves on 3
                 prompt(searchMove);
                 break;
-            case "4" : // Quits searching on 4
+            case "4": // Quits searching on 4
                 exit("Quit out");
                 break;
             default:
